1b61e8fb0ab2cbdcd32e281233370301ee40dd83
nathanstouffer <nathanstouffer1999@gmail.com>
2026-02-06 21:38:16 -0700

    rename project

---
+++ Cribbot/Cribbot/CribbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  CribbotApp.swift
+//  Cribbot
+//
+//  Created by Nathan Stouffer on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct CribbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
