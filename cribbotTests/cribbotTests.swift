e7d372db171a0850a8a85507d62e4a3329a20baa
nathanstouffer <nathanstouffer1999@gmail.com>
2026-02-06 18:01:33 -0700

    create cribbot xcode project

---
+++ cribbotTests/cribbotTests.swift
@@ -0,0 +1,17 @@
+//
+//  cribbotTests.swift
+//  cribbotTests
+//
+//  Created by Nathan Stouffer on 2/6/26.
+//
+
+import Testing
+@testable import cribbot
+
+struct cribbotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
