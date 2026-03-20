baf7d92c561612f77959ccf88a19231b9e71a8e1
nathanstouffer <nathanstouffer1999@gmail.com>
2026-03-19 20:01:49 -0600

    introduce test framework for scoring

--- Cribbot/Scoring.swift
+++ Cribbot/Scoring.swift
@@ -1,17 +1,17 @@
-func score(hand: Array<Card>, flip: Card, isCrib: Bool) -> Int {
-  var allCards = hand
-  allCards.append(flip)
+func score(hand: [Card], flip: Card, isCrib: Bool) -> Int {
+  var cards = hand
+  cards.append(flip)
 
   var score = 0
-  score += scoreFromPairs(cards: allCards)
-  score += scoreFromFifteens(cards: allCards)
-  score += scoreFromRuns(cards: allCards)
-  score += scoreFromFifteens(cards: cards, flip: flip, isCrib: isCrib)
-  score += scoreFromNobs(cards, flip)
+  score += scoreFromPairs(cards: cards)
+  score += scoreFromFifteens(cards: cards)
+  score += scoreFromRuns(cards: cards)
+  score += scoreFromFlush(cards: hand, flip: flip, isCrib: isCrib)
+  score += scoreFromNobs(hand: hand, flip: flip)
   return score
 }
 
-func scoreFromPairs(cards: Array<Card>) -> Int {
+func scoreFromPairs(cards: [Card]) -> Int {
   var score = 0
   for i in 0..<cards.count {
     for j in i..<cards.count {
@@ -25,19 +25,19 @@ func scoreFromPairs(cards: Array<Card>) -> Int {
   return score
 }
 
-func scoreFromFifteens(cards: Array<Card>) -> Int {
-  
+func scoreFromFifteens(cards: [Card]) -> Int {
+  return 0
 }
 
-func scoreFromRuns(cards: Array<Card>) -> Int {
-  
+func scoreFromRuns(cards: [Card]) -> Int {
+  return 0
 }
 
-func scoreFromFlush(cards: Array<Card>, flip: Card, isCrib: Bool) -> Int {
-  
+func scoreFromFlush(cards: [Card], flip: Card, isCrib: Bool) -> Int {
+  return 0
 }
 
-func scoreFromNobs(hand: Array<Card>, flip: Card) -> Int {
+func scoreFromNobs(hand: [Card], flip: Card) -> Int {
   for card in hand {
     if card.rank == Card.Rank.jack && card.suit == flip.suit {
       return 1
--- CribbotTests/CribbotTests.swift
+++ CribbotTests/CribbotTests.swift
@@ -4,8 +4,27 @@ import Testing
 
 struct CribbotTests {
 
-  @Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  struct ScoreTest {
+    let hand: [Card]
+    let flip: Card
+    let isCrib: Bool
+    let score: Int
+  }
+
+  @Test func testScore() throws {
+    var tests = Array<ScoreTest>()
+
+    tests.append(
+      ScoreTest(
+        hand: [Card(rank: .ace, suit: .clubs)], flip: Card(rank: .king, suit: .diamonds),
+        isCrib: false, score: 0)
+    )
+
+    for i in 0..<tests.count {
+      let test = tests[i]
+      let score = score(hand: test.hand, flip: test.flip, isCrib: test.isCrib)
+      #expect(test.score == score, "Failed test index \(i)")
+    }
   }
 
 }
