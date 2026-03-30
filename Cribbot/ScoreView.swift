94e9d4cc68601a812e9c621dd5a7ce110ad4d322
nathanstouffer <nathanstouffer1999@gmail.com>
2026-03-29 20:13:38 -0600

    formatting

--- Cribbot/ScoreView.swift
+++ Cribbot/ScoreView.swift
@@ -1,20 +1,20 @@
 import SwiftUI
 
 struct ScoreView: View {
-  
+
   var computer: Player
   var human: Player
-  
-    var body: some View {
-      HStack {
-        Spacer()
-        score(text: "Opponent: \(computer.score)")
-        Spacer()
-        score(text: "You: \(human.score)")
-        Spacer()
-      }
+
+  var body: some View {
+    HStack {
+      Spacer()
+      score(text: "Opponent: \(computer.score)")
+      Spacer()
+      score(text: "You: \(human.score)")
+      Spacer()
     }
-  
+  }
+
   private func score(text: String) -> some View {
     Text(text)
       .foregroundStyle(.white)
